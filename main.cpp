12ca9422fba4e40a5631b8730d334fc8aa175f59
Khaled Ismaeel <k.ismaeel@innopolis.university>
2021-10-22 23:49:31 +0300

    treat IP rules as integer ranges

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-// #define LITTLE
+//#define LITTLE
 
 namespace schema {
     enum dimension_type {
@@ -23,85 +23,72 @@ namespace data {
 #else
     const size_t IP_SIZE = 32;
 #endif
-    typedef array<char, IP_SIZE> ip;
-    typedef unsigned short port;
-
     size_t n, m;
     const size_t MAXN = 150000;
     const size_t MAXM = 15000;
 
-    array<array<ip, schema::MAXD>, MAXN> ip_rules;
-    array<array<pair<port, port>, schema::MAXD>, MAXN> port_rules;
-
-    array<array<ip, schema::MAXD>, MAXM> ip_keys;
-    array<array<port, schema::MAXD>, MAXN> port_keys;
+    array<array<pair<unsigned int, unsigned int>, schema::MAXD>, MAXN> rules;
+    array<array<unsigned int, schema::MAXD>, MAXM> keys;
 }
 
 namespace utils {
-    ostream &operator<<(ostream &out, const data::ip &arr) {
-        for (size_t i = 0; i < schema::MAXD; i++)
-            out << arr[i];
-        return out;
-    }
-
-    bool match(const data::ip &a, const data::ip &b) {
-        for (int i = 0; i < data::IP_SIZE; i++) {
-            if (a[i] == '*')
-                return true;
-            if (a[i] == '0' and b[i] == '1')
+    bool match(const array<pair<unsigned int, unsigned int>, schema::MAXD> &rule,
+               const array<unsigned int, schema::MAXD> &key) {
+        for (size_t i = 0; i < schema::n_dims; i++)
+            if (key[i] < rule[i].first or key[i] > rule[i].second)
                 return false;
-            if (a[i] == '1' and b[i] == '0')
-                return false;
-        }
         return true;
     }
 
-    bool match(const pair<data::port, data::port> &a, const data::port &b) {
-        return b >= a.first and b <= a.second;
-    }
 }
 
 namespace parsing {
-    pair<array<data::ip, schema::MAXD>, array<pair<data::port, data::port>, schema::MAXD>>
-    parse_rule(const vector<string> &tokens) {
-        size_t ip_idx = 0, port_idx = 0;
-        array<data::ip, schema::MAXD> ips;
-        array<pair<data::port, data::port>, schema::MAXD> ports;
-        for (size_t i = 0; i < schema::n_dims; ++i) {
-            const auto &token = tokens[i];
-            if (schema::dims[i] == schema::dimension_type::IP) {
-                for (size_t j = 0; j < data::IP_SIZE; j++)
-                    ips[ip_idx][j] = token[j];
-                ip_idx++;
-            } else {
-                auto div = token.find('-');
-                auto l = (unsigned short) stoi(token.substr(0, div));
-                auto r = (unsigned short) stoi(token.substr(div + 1));
-                ports[port_idx++] = {l, r};
+    pair<unsigned int, unsigned int> parse_rule_token(const string &token, const schema::dimension_type &type) {
+        unsigned int l = 0, r = 0;
+        if (type == schema::dimension_type::IP) {
+            for (const auto &c: token) {
+                l <<= 1, r <<= 1;
+                if (c == '*')
+                    r |= 1;
+                else {
+                    l |= (c - '0');
+                    r |= (c - '0');
+                }
             }
+        } else {
+            auto div = token.find('-');
+            l = (unsigned short) stoi(token.substr(0, div));
+            r = (unsigned short) stoi(token.substr(div + 1));
         }
-        return {ips, ports};
+        return {l, r};
     }
 
-    pair<array<data::ip, schema::MAXD>, array<data::port, schema::MAXD>>
+    unsigned int parse_key_token(const string &token, const schema::dimension_type &type) {
+        unsigned int ret = 0;
+        if (type == schema::dimension_type::IP)
+            ret = (unsigned int) stoul(token, nullptr, 2);
+        else
+            ret = (unsigned int) stoul(token);
+        return ret;
+    }
+
+    array<pair<unsigned int, unsigned int>, schema::MAXD>
+    parse_rule(const vector<string> &tokens) {
+        array<pair<unsigned int, unsigned int>, schema::MAXD> ranges;
+        for (size_t i = 0; i < schema::n_dims; ++i)
+            ranges[i] = parse_rule_token(tokens[i], schema::dims[i]);
+        return ranges;
+    }
+
+    array<unsigned int, schema::MAXD>
     parse_key(const vector<string> &tokens) {
-        size_t ip_idx = 0, port_idx = 0;
-        array<data::ip, schema::MAXD> ips;
-        array<data::port, schema::MAXD> ports;
-        for (size_t i = 0; i < schema::n_dims; ++i) {
-            const auto &token = tokens[i];
-            if (schema::dims[i] == schema::dimension_type::IP) {
-                for (size_t j = 0; j < data::IP_SIZE; j++)
-                    ips[ip_idx][j] = token[j];
-                ip_idx++;
-            } else
-                ports[port_idx++] = stoi(token);
-        }
-        return {ips, ports};
+        array<unsigned int, schema::MAXD> ret;
+        for (size_t i = 0; i < schema::n_dims; ++i)
+            ret[i] = parse_key_token(tokens[i], schema::dims[i]);
+        return ret;
     }
 
     void parse_input() {
-        using utils::operator<<;
         ios_base::sync_with_stdio(false);
         cin.tie(nullptr);
         cout.tie(nullptr);
@@ -143,18 +130,14 @@ namespace parsing {
                     schema::n_ports++;
                 }
             }
-            auto rule = parsing::parse_rule(tokens);
-            data::ip_rules[0] = rule.first;
-            data::port_rules[0] = rule.second;
+            data::rules[0] = parse_rule(tokens);
         }
 
         for (size_t i = 1; i < data::n; i++) {
             vector<string> tokens(schema::n_dims);
             for (size_t j = 0; j < schema::n_dims; j++)
                 cin >> tokens[j];
-            auto rule = parsing::parse_rule(tokens);
-            data::ip_rules[i] = rule.first;
-            data::port_rules[i] = rule.second;
+            data::rules[i] = parse_rule(tokens);
         }
 
         cin >> data::m;
@@ -163,9 +146,7 @@ namespace parsing {
             vector<string> tokens(schema::n_dims);
             for (size_t j = 0; j < schema::n_dims; j++)
                 cin >> tokens[j];
-            auto key = parsing::parse_key(tokens);
-            data::ip_keys[i] = key.first;
-            data::port_keys[i] = key.second;
+            data::keys[i] = parse_key(tokens);
         }
     }
 }
@@ -176,22 +157,7 @@ int main() {
     for (int i = 0; i < data::m; i++) {
         int ans = -1;
         for (int j = 0; j < data::n; j++) {
-            bool found = true;
-            for (int k = 0; k < schema::n_ips; k++) {
-                if (not utils::match(data::ip_rules[j][k], data::ip_keys[i][k])) {
-                    found = false;
-                    break;
-                }
-            }
-            if (not found)
-                continue;
-            for (int k = 0; k < schema::n_ports; k++) {
-                if (not utils::match(data::port_rules[j][k], data::port_keys[i][k])) {
-                    found = false;
-                    break;
-                }
-            }
-            if (found) {
+            if (utils::match(data::rules[j], data::keys[i])) {
                 ans = j;
                 break;
             }
